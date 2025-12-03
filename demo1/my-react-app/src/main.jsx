65c8ba7c34c28f6353a9fd8fb02c1d7de0f6fb49
黄思维 <14870082+huang-siwei0911@user.noreply.gitee.com>
2025-12-03 23:14:37 +0800

    第二次提交

--- demo1/my-react-app/src/main.jsx
+++ demo1/my-react-app/src/main.jsx
@@ -4,9 +4,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import App2 from './App2.jsx'
 import App3 from './App3.jsx'
+import App4 from './App4.jsx'
+import App5 from './App5.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App3 />
+    <App5 />
   </StrictMode>,
 )
